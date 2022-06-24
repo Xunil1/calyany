d3bca5d6efad969d3f686bf12fd59bbd9f8e9159
nekrulya <nekrulyaa@mail.ru>
2022-06-24 22:33:19 +0500

    big update js started imgs added css divided

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,32 @@
-var cart
+let cart = {
+
+}
+
+
+//увеличение количества товара 
+//уменьшение количества товара
+//удаление товара 
+
+
+document.onclick = event => {
+    console.log(event.target.classList)
+    if (event.target.classList.contains('add')) {
+        addFunction(event.target.dataset.id);
+    }
+}
+
+const addFunction = id => {
+	if (cart[id] >= 1) {
+		cart[id]++
+	}
+	else {
+		cart[id] = 1
+	}
+    renderCart()
+}
+
+const renderCart = () => {
+    console.log(cart)
+}
+
+renderCart()
