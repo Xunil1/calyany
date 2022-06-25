cc42fe2c50305b7c78d19cc669f90144076b5fe8
nekrulya <nekrulyaa@mail.ru>
2022-06-25 20:36:08 +0500

    Форма добавления товара

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ db = SQLAlchemy(app)
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    surname = db.Column(db.String(100), nullable=False)
+    surname = db.Column(db.String(100), nullable=False) 
     username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
