c79182db9468a95f832077e85cf5fb3617e6ce4f
Xunil1 <ka.po.po@yandex.ru>
2022-06-25 18:41:02 +0500

    товары в cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -21,6 +21,15 @@ document.onclick = event => {
     if (event.target.classList.contains('cart_show')) {
         $("#cart").toggle(200);
     }
+    if (event.target.classList.contains('minus')) {
+        minusFunction(event.target.dataset.id);
+    }
+    if (event.target.classList.contains('plus')) {
+        plusFunction(event.target.dataset.id);
+    }
+    if (event.target.classList.contains('delete')) {
+        deleteFunction(event.target.dataset.id);
+    }
 }
 
 
@@ -39,12 +48,33 @@ const addFunction = id => {
     renderCart()
 }
 
+const minusFunction = id => {
+	if (cart[id] === 1) {
+		cart[id] = 1
+	}
+	else {
+		cart[id] -= 1
+	}
+    renderCart()
+}
+
+const plusFunction = id => {
+	cart[id]++
+    renderCart()
+}
+
+const deleteFunction = id => {
+	delete cart[id]
+    renderCart()
+}
+
 const renderCart = () => {
     if (Object.keys(cart).length > 0){
         $('#cart__mini').show()
     }
     else{
         $('#cart__mini').hide()
+        $('#cart').hide()
     }
     console.log(cart)
     $('.cart_fullprice').empty()
@@ -58,20 +88,21 @@ const renderCart = () => {
 	Object.keys(cart).map(function(cart_el) {
 		if (cart_el === "calyan"){
 			html_products += '<div class="cart_el" id="cart_el"><div class="cart_el_title">Кальян</div>'
-            html_products += '<div class="cart_el_count"><div class="cart_el_button minus">–</div><div class="cart_el_count_number">' + cart[cart_el] + '</div><div class="cart_el_button plus">+</div></div><div class="cart_el_price">' + cart[cart_el] * pricing[cart_el] + ' GEL</div>'
-            html_products += '<div class="cart_el_button delete">×</div></div>'
+            html_products += '<div class="cart_el_count"><div class="cart_el_button minus" data-id="'+ cart_el +'">–</div><div class="cart_el_count_number">' + cart[cart_el] + '</div><div class="cart_el_button plus" data-id="'+ cart_el +'">+</div></div><div class="cart_el_price">' + cart[cart_el] * pricing[cart_el] + ' GEL</div>'
+
 
 			price += cart[cart_el] * pricing[cart_el]
 		}
 		else if (cart_el === "extra-cup"){
-			html_products += '<div class="cart_el" id="cart_el"><div class="cart_el_title">Дополнительная забивкаДополнительная забивка</div>'
-            html_products += '<div class="cart_el_count"><div class="cart_el_button minus">–</div><div class="cart_el_count_number">' + cart[cart_el] + '</div><div class="cart_el_button plus">+</div></div><div class="cart_el_price">' + cart[cart_el] * pricing[cart_el] + ' GEL</div>'
-            html_products += '<div class="cart_el_button delete">×</div></div>'
+			html_products += '<div class="cart_el" id="cart_el"><div class="cart_el_title">Дополнительная забивка</div>'
+            html_products += '<div class="cart_el_count"><div class="cart_el_button minus" data-id="'+ cart_el +'">–</div><div class="cart_el_count_number">' + cart[cart_el] + '</div><div class="cart_el_button plus" data-id="'+ cart_el +'">+</div></div><div class="cart_el_price">' + cart[cart_el] * pricing[cart_el] + ' GEL</div>'
 			price += cart[cart_el] * pricing[cart_el]
 		}
 		else{
-			html_products += '<div id="cart_el">'+ json[cart_el] + ' Количество: ' + cart[cart_el] + '</div>'
+		    html_products += '<div class="cart_el" id="cart_el"><div class="cart_el_title">'+ json[cart_el] + '</div>'
+            html_products += '<div class="cart_el_count"><div class="cart_el_button minus" data-id="'+ cart_el +'">–</div><div class="cart_el_count_number">' + cart[cart_el] + '</div><div class="cart_el_button plus" data-id="'+ cart_el +'">+</div></div><div class="cart_el_price">' + 0 + ' GEL</div>'
 		}
+		html_products += '<div class="cart_el_button delete" data-id="'+ cart_el +'">×</div></div>'
 		html_products_inputs += '<input type="hidden" name="order_' + cart_el + '" id="order_' + cart_el + '" value="' + cart[cart_el] + '">'
 	    
 	});
